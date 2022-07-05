c66c75051a255e199b83a63b80591a80c87c23ec
essennejaye <sjeffers02@gmail.com>
2022-07-04 22:00:40 -0700

    get word and check answer functionality

--- public/js/gamePlay.js
+++ public/js/gamePlay.js
@@ -1,14 +1,35 @@
-(function pageInit() {
-  const blankInput = document.getElementById('word');
-  blankInput.addEventListener('click', getWordInput);
-})();
+// (function pageInit() {
+//   getWordInput();
+// })();
 
-function getWordInput(e) {
-  e.preventDefault();
+let englishAnswer = '';
+
+const newWord = document.getElementById('next-btn');
+newWord.addEventListener('click', getWordInput);
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', checkAnswer);
+const rightAnswer = document.getElementById('response');
+
+function getWordInput() {
   fetch('/api/getPinWord')
     .then((response) => response.json())
     .then((data) => {
       const wordInput = document.getElementById('word');
       wordInput.value = data.pinyin;
+      englishAnswer = data.english;
     });
+}
+
+function checkAnswer() {
+  let answer = document.getElementById('answer').value;
+
+  if (!answer) {
+    alert('You must enter an English transation');
+    return;
+  }
+  if (answer === englishAnswer) {
+    rightAnswer.innerHTML = 'Correct';
+  } else {
+    rightAnswer.innerHTML = 'Incorrect';
+  }
 }
