ce061f71ada3864edc8ea1452b50e7555361d041
essennejaye <sjeffers02@gmail.com>
2022-08-28 10:50:52 -0700

    sigh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const db = require('./configs/connection');
 const express = require('express');
-const routes = require('./router');
+// const routes = require('./router');
 const path = require('path');
 
 const app = express();
@@ -10,12 +10,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/api/', routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+app.get('/api/test-endpoint', (req, res) => {
+  res.status(200).json({ id: 1, text: 'Test Endpoint Description' });
 });
+
+// app.use('/api/', routes);
+
+// db.once('open', () => {
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+// });
 
 module.exports = app;
