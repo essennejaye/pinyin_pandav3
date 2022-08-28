babe088db4e2d6bb2a1495510dba15e2af33e7fc
essennejaye <sjeffers02@gmail.com>
2022-08-28 12:13:37 -0700

    sigh for the umpteenth and 3 time

--- api/dictionary.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-const route = require('../routes/dictionary');
-app.use('/api/', route);
-
-module.exports = app;
--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-// const db = require('./configs/connection');
-const express = require('express');
+// // const db = require('./configs/connection');
+// const express = require('express');
 // const route = require('./routes/dictionary');
-// const path = require('path');
+// // const path = require('path');
 
-const app = express();
+// const app = express();
 
-// app.use(express.static(path.join(__dirname, 'public', 'html')));
-// app.use(express.static('public'));
+// // app.use(express.static(path.join(__dirname, 'public', 'html')));
+// // app.use(express.static('public'));
 
 // const PORT = process.env.PORT || 3000;
 
-// app.use('/api/', route);
+// // app.use('/api/', route);
 
 // db.once('open', () => {
 //   app.listen(PORT, () => {
 //     console.log(`Server listening on ${PORT}`);
 //   });
 // });
+
+// module.exports = app;
+
+const db = require('./configs/connection');
+require('dotenv').config();
+const express = require('express');
+// const Dictionary = require('../models/Dictionary');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('dictionary', (req, res) => {
+  res.status(200).json({ id: 1, text: 'Test Endpoint Description' });
+});
+
+// router.get('/dict_entries', (req, res) => {
+//   Dictionary.aggregate([{ $sample: { size: 500 } }], function (err, results) {
+//     try {
+//       res.send(results);
+//     } catch (err) {
+//       res.status(500).send(err);
+//     }
+//   });
+// });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+});
 
 module.exports = app;
