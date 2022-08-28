20175c2c5986cd924662e2539e3f0cf138edbb62
essennejaye <sjeffers02@gmail.com>
2022-08-27 17:04:57 -0700

    updating vercel config

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const app = require('../server');
-const db = require('../configs/connection');
+const db = require('./configs/connection');
 const express = require('express');
 const routes = require('./router');
 const path = require('path');
@@ -8,24 +8,10 @@ const { v4 } = require('uuid');
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
 // app.use(express.static(path.join(__dirname, 'public', 'html')));
 // app.use(express.static('public'));
 
 app.use('/', routes);
-
-module.exports = app;
 
 const PORT = process.env.PORT || 3000;
 
@@ -34,3 +20,5 @@ db.once('open', () => {
     console.log(`Server listening on ${PORT}`);
   });
 });
+
+module.exports = app;
